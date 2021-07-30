cf738f5a0595a4d2e2d28acb6f655e1e53632b96
Emmanuel <emanuelnavas@hotmail.com>
2021-07-30 16:48:55 -0300

    UPDATE

--- checkpoint.js
+++ checkpoint.js
@@ -15,9 +15,9 @@ const {
 
 // ----------------------
 
-// ----- Recursión -----
+//* ----- Recursión -----
 
-// EJERCICIO 1
+//* EJERCICIO 1 --> OK
 // Implementar la función isAncestor: debe determinar si dado dos nombres de personas las mismas
 // son parientes o no (La primera debe ser ancestro de la segunda). La función recibira un objeto
 // que va a representar sólo la parte femenina del "arbol genealogico" familiar y será de la siguiente forma:
@@ -37,13 +37,40 @@ const {
 //  - Caso que devuelve false --> isAncestor(genealogyTree, "Jacqueline Bouvier", "Abigail Simpson")
 //  [Observar los tests para otros casos]
 
-var isAncestor = function(genealogyTree, ancestor, descendant){
-  // Tu código aca:
+var isAncestor = function(genealogyTree, ancestor, descendant) {
 
+  // console.log(genealogyTree[ancestor].length)
+
+  var tamañoArray = genealogyTree[ancestor].length //* tomo el valor para luego saber la longitud a iterar
+  
+  // console.log(genealogyTree[ancestor])
+  // console.log(genealogyTree[ancestor][index])
+
+  for (var index = 0; index < tamañoArray; index++) { //* uso un for para iterar dentro
+ 
+    // console.log(genealogyTree[genealogyTree[ancestor][index]].length > 0)
+    if (genealogyTree[genealogyTree[ancestor][index]].length > 0) { //* si la propiedad no esta vacia (osea que tiene descendientes)
+
+      return isAncestor(genealogyTree, genealogyTree[ancestor][index], descendant); //* aplico recursividad para iterar por los array del objeto
+
+    }
+
+    // console.log(genealogyTree[ancestor][index] === descendant)
+    else if (genealogyTree[ancestor][index] === descendant) { //* si lo encuentra, retorna TRUE
+
+      return true;
+
+    }
+
+  }
+// console.log(genealogyTree[ancestor].length === 0)
+  if (genealogyTree[ancestor].length === 0)  return false //* caso base, corta la iteracion si no hay coincidencias, y retorna false
+
+  return false
 }
 
 
-// EJERCICIO 2
+//* EJERCICIO 2 --> PENDIENTE
 // Secuencia inventada: f(n) = f(n-1) x f(n-2) - f(n-2)
 // Siendo f, secuenciaHenry.
 // Donde las primeras dos posiciones son dadas por el parametro recibidos y a partir de
@@ -82,9 +109,9 @@ function secuenciaHenry(obj, n) {
 
 // ---------------------
 
-// ----- LinkedList -----
+//* ----- LinkedList -----
 
-// EJERCICIO 3
+//* EJERCICIO 3 --> OK
 // Implementar el método size dentro del prototype de LinkedList que deberá retornar el tamaño actual de
 // la LinkedList. En el caso de que la lista se encuentre vacía deberá retornar cero.
 // Ejemplo:
@@ -98,11 +125,22 @@ function secuenciaHenry(obj, n) {
 
 LinkedList.prototype.size = function(){
   // Tu código aca:
+  var puntero = this.head; //* creo una variable que voy a usar de puntero
+
+  var contador = 0; //* creo una variable contador, que se incrementa con cada iteracion del While
+
+  while (puntero != null) { //*mientras pueda mover un lugar el puntero avanzo, cuando llego al final, puntero === NULL y no entra mas
+
+      contador++; //* incremento el contador en cada ciclo While
+
+      puntero = puntero.next; //* avanzo un lugar el puntero, avanzo en la linkedlist, y evito un loop infinito cuando puntero apunta a null
+  }
+  return contador;//* retorno el contador con la cantidad de nodos.
 
 }
 
 
-// EJERCICIO 4
+//* EJERCICIO 4 --> OK
 // Implementar el método switchPos dentro del prototype de LinkedList que deberá intercambiar
 // el elemento que se encuentre en pos1 con el elemento en pos2
 // En el caso de que alguna de las dos posiciones no sea válida (Supere el tamaño de la lista actual 
@@ -120,9 +158,59 @@ LinkedList.prototype.size = function(){
 LinkedList.prototype.switchPos = function(pos1, pos2){
   // Tu código aca:
 
+  if (!this.head) return false //* si la lista esta vacia retorno False
+
+  var contador = this.size() //* utilizo el metodo creado SIZE para validar el largo de la lista
+  // console.log(contador)
+  
+  if (contador <= pos1 || contador <= pos2) return false //* chequeo las posiciones existan, y no sobrepasen el tamaño de la lista
+  if (pos1 < 0 || pos2 < 0) return false; //* si la posicion pasada por argumento es negativa, retorno FALSE
+
+  var nodoPos1 = this.head; //* declaro variable para manejar la posicion posterior del nodo1 
+  var nodoPos2 = this.head; //* declaro variable para manejar la posicion posterior del nodo2
+  
+  var nodoAnt1; //* declaro variable para manejar la posicion anterior del nodo1
+  var nodoAnt2; //* declaro variable para manejar la posicion anterior del nodo2
+  
+  for (let i=1; i <= pos1; i++) { //*igualo la posicion del notoAnt1 con nodoPos1, y el nodo posterior avanza un lugar.
+    nodoAnt1 = nodoPos1;
+    nodoPos1 = nodoPos1.next;
+    // console.log(nodoAnt1)
+    // console.log(nodoPos1)
+  }
+  
+  for (let i=1; i <= pos2; i++) {//*igualo la posicion del notoAnt2 con nodoPos2, y el nodo posterior avanza un lugar.
+    nodoAnt2 = nodoPos2;
+    nodoPos2 = nodoPos2.next;
+    // console.log(nodoAnt2)
+    // console.log(nodoPos2)
+  }
+  
+  //*verifico la posicion de cada marcador y si tiene head o no. Sino asigno al head al nodoPos2
+  if (nodoAnt1) {
+    nodoAnt1.next = nodoPos2;
+  } else {
+    this.head = nodoPos2;
+  }
+  
+ //*verifico la posicion de cada marcador y si tiene head o no. Sino asigno al head al nodoPos1
+  if (nodoAnt2) {
+    nodoAnt2.next = nodoPos1;
+  } else {
+    this.head = nodoPos1;
+  }
+
+  //* hago el intercambio entre nodo1 y nodo2 (SWITCH)
+  var temporal = nodoPos1.next; //* guardo el siguiente en una variable para no perderlo
+  nodoPos1.next = nodoPos2.next; //* reemplazo el de 1ro con el 2do
+  nodoPos2.next = temporal; //* y al 2do la variable tempraria
+
+  return true //* como se efecto correctamente, retorno TRUE
+
 }
 
-// EJERCICIO 5
+//* EJERCICIO 5 --> OK
+
 // Implementar la función mergeLinkedLists que, a partir de dos listas simplemente enlazadas 
 // del mismo tamaño retorne una nueva lista con los elementos de ambas listas
 // Ejemplo:
@@ -133,18 +221,36 @@ LinkedList.prototype.switchPos = function(pos1, pos2){
 // Nota: las listas enlazadas mergeadas intercalandose.
 // El nodo 1 de la lista 1, se conecta con el nodo 1 de la lista 2.
 // Continuando con el nodo 2 de la lista 2, conectandose con el nodo 2 de la lista 2.
-var mergeLinkedLists = function(linkedListOne, linkedListTwo){
-  // Tu código aca:
 
-}
+
+var mergeLinkedLists = function (linkedListOne, linkedListTwo) {
+  // Tu código aca:
+  const mergedList = new LinkedList(); //* creo una nueva Linkedlist, para luego ir agregando los nodos intercalados
+
+  var lista1 = linkedListOne.head  //*creo una lista1 con la lista pasada como argumento
+  // console.log(lista1) 
+  var lista2 = linkedListTwo.head //*creo una lista2 con la lista pasada como argumento
+  // console.log(linkedListTwo.head)
+
+  while (lista1 && lista2) { //*mientras tenga elementos en ambas listas sigo iterando (no se rompe porque aclaran que son iguales)
+
+    mergedList.add(lista1.value) //* agrego un nodo a la lista1
+    lista1 = lista1.next; //*avanzo un nodo, para seguir iterando
+
+    mergedList.add(lista2.value) //* agrego un nodo a la lista2
+    lista2 = lista2.next; //*avanzo un nodo, para seguir iterando
+  }
+  // console.log(mergedList)
+  return mergedList //* retorno la lista mergeada
+};
 
 
 // ----------------------
 
 
-// ----- QUEUE -----
+//* ----- QUEUE -----
 
-// EJERCICIO 6
+//* EJERCICIO 6 --> OK
 // Implementar la función cardGame: a partir de dos Queues que va a recibir como paráemtro que
 // van a representar mazos de cartas de dos jugadores debemos determinar quien va a ser el ganador
 // de este juego que va a tener la siguiente dinámica:
@@ -181,17 +287,52 @@ var mergeLinkedLists = function(linkedListOne, linkedListTwo){
 // finalizar el juego.
 
 
-var cardGame = function(playerOneCards, playerTwoCards){
+var cardGame = function(playerOneCards, playerTwoCards) {
   // Tu código aca:
+
+  var puntosCastillo1 = 100; //* declaro una variable con los puntos del castillo 1
+  var puntosCastillo2 = 100; //* declaro una variable con los puntos del castillo 2
+
+  while (playerOneCards.size() > 0 && playerTwoCards.size() > 0) { //* utilizo el metodo size(), para saber hasta donde sigo sacando cartas., sino salgo.
+
+    //* Saco de la cola las 2 primeras cartas Jugador 1
+    var cartaJugador1Ataque = playerOneCards.dequeue() 
+    var cartaJugador1Defensa = playerOneCards.dequeue()
+    
+    //* Saco de la cola las 2 primeras cartas del Jugador 2
+    var cartaJugador2Ataque = playerTwoCards.dequeue()
+    var cartaJugador2Defensa = playerTwoCards.dequeue()
+    
+    //* Aplico el Ataque del Jugador 1 al Castillo del Jugador 2
+    var ataquejugador1 = cartaJugador2Defensa.defense - cartaJugador1Ataque.attack
+    if (ataquejugador1 < 0) { //* La diferencia de los puntos, se aplican al castillo 2
+      puntosCastillo2 += ataquejugador1
+      // console.log(ataquejugador1)
+      // console.log("puntos castillo2 : " + puntosCastillo2)
+    }
+  
+    //* Aplico el Ataque del Jugador 2 al Castillo del Jugador 1
+    var ataqueJugador2 = cartaJugador1Defensa.defense - cartaJugador2Ataque.attack
+    if (ataqueJugador2 < 0) { //* La diferencia de los puntos, se aplican al castillo 1
+      puntosCastillo1 += ataqueJugador2
+      // console.log(ataqueJugador2)
+      // console.log("puntos castillo1 : " + puntosCastillo1)
+    }
+
+  }
+  
+  if (puntosCastillo1 > puntosCastillo2) return "PLAYER ONE" //* si los puntos del castillo 2 son mayores al castillo 1, gana el Jugador 1
+  if(puntosCastillo1 < puntosCastillo2) return "PLAYER TWO" //* si los puntos del castillo 1 son menores al castillo 2, gana el Jugador 2
+  else return "TIE" //* sino es un empate
 
 }
 
 // ---------------
 
 
-// ----- BST -----
+//* ----- BST -----
 
-// EJERCICIO 7
+//* EJERCICIO 7 --> OK
 // Implementar la función height dentro del prototype de BinarySearchTree que debe devolver la "altura"
 // máxima del arbol recibido por parámetro.
 // Ejemplo:
@@ -205,8 +346,18 @@ var cardGame = function(playerOneCards, playerTwoCards){
 // Este arbol tiene una altura de 4
 // PISTA: Una forma de resolverlo es pensarlo recursivamente y usando Math.max
 
-BinarySearchTree.prototype.height = function(){
+BinarySearchTree.prototype.height = function() {
   // Tu código aca:
+
+  if(!this.value) return 0 //* si no hay nodos retorno 0
+  
+  if(!this.left && !this.right) return 1 //* pregunto si no tiene nodos a su izquierda y a su derecha, y si es asi, esta solo, retorno 1
+
+  if(!this.right) return 1 + this.left.height() //* si no tiene nodos a su derecha, utilizo recursiva hacia la izquierda
+
+  if(!this.left) return 1 + this.right.height() //* si no tiene nodos a su izquierda, utilizo recursiva hacia la derecha
+
+  return 1 + Math.max(this.left.height(), this.right.height()) //* con la pista del enunciado, utilizo el metodo Math.max para comparar las dos ramas, y para el HEIGHT (NIVEL) de la mas profunda
 
 }
 
@@ -214,7 +365,7 @@ BinarySearchTree.prototype.height = function(){
 // ---------------
 
 
-// Ejercicio 8
+//* Ejercicio 8
 // Dado un arreglo ordenado, encontrar el índice de un elemento específico pasado como parámetro
 // utilizando el método conocido como búsqueda binaria. En el caso de que el número buscado no se encuentre
 // en el array devolver -1.
@@ -232,7 +383,7 @@ var binarySearch = function (array, target) {
 
 }
 
-// EJERCICIO 9
+//* EJERCICIO 9
 // Ordená un arreglo de objetos usando un bubble sort pero con algunas particularidades.
 // Además del arreglo a ordenar (array) la función va a recibir como parámetro una función
 // que va a ser quien va a determinar si un elemento es "mayor" al otro para determinar su
@@ -260,9 +411,9 @@ var specialSort = function(array, orderFunction) {
 
 }
 
-// ----- Closures -----
+//* ----- Closures -----
 
-// EJERCICIO 10
+//* EJERCICIO 10
 // Implementar la función closureDetect que recibe como parámetro:
 //  - Un array (symptoms) que va a contener en cada posición un string representando un
 //    síntoma médico de alguna enfermedad
